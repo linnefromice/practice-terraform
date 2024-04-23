9dd3a4f67f367375ec87ab3465f3df2c829af146
linnefromice <linnefromice@gmail.com>
2024-04-23 12:20:31 +0900

    tutorial: gcp

--- developer.hashicorp.com/learn-tf-gcp/main.tf
+++ developer.hashicorp.com/learn-tf-gcp/main.tf
@@ -9,8 +9,25 @@ terraform {
 
 provider "google" {
   project = "salse-tech"
+  zone = "us-east1-b"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {}
+  }
 }
