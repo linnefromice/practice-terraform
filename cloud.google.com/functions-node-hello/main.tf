97ba6d7d80fd0e9b7fd567762e7cb54c6106bd7e
linnefromice <linnefromice@gmail.com>
2024-04-25 16:53:37 +0900

    tutorial gcp (functions): storage bucket

--- cloud.google.com/functions-node-hello/main.tf
+++ cloud.google.com/functions-node-hello/main.tf
@@ -1,1 +1,5 @@
-
+resource "google_storage_bucket" "default" {
+  name                        = "gcp-tutorial-gcf-source"
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
