802972212a32e70dbff826dc1d75cc70795e27e5
linnefromice <linnefromice@gmail.com>
2024-04-25 21:50:25 +0900

    tutorial gcp (functions): cloud function 2

--- cloud.google.com/functions-node-hello/main.tf
+++ cloud.google.com/functions-node-hello/main.tf
@@ -5,9 +5,9 @@ resource "google_storage_bucket" "default" {
 }
 
 data "archive_file" "default" {
-  type        = "zip"
-  source_file = "functions/function-source.zip"
-  # source_dir  = "functions"
+  type = "zip"
+  # source_file = "functions/function-source.zip"
+  source_dir  = "functions/function-source"
   output_path = "tmp/source.zip"
 }
 # data "archive_file" "zip_temp_1" {
@@ -26,3 +26,36 @@ resource "google_storage_bucket_object" "object" {
 #   bucket = google_storage_bucket.default.name
 #   source = data.archive_file.zip_temp_1.output_path
 # }
+
+resource "google_cloudfunctions2_function" "default" {
+  name     = "gcp-tutorial-gcf"
+  location = "us-central1"
+
+  build_config {
+    runtime     = "nodejs16"
+    entry_point = "helloHttp"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "default" {
+  location = google_cloudfunctions2_function.default.location
+  service  = google_cloudfunctions2_function.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.default.service_config[0].uri
+}
